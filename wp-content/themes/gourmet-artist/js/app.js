ced6e505c154c0710818cf849b5c1fce36d906c6
Miguel Zac <miguel.quispe@frctal.com.pe>
2018-11-15 10:46:12 -0500

    37. Agregando Javascript a nuestro ejemplo.

--- wp-content/themes/gourmet-artist/js/app.js
+++ wp-content/themes/gourmet-artist/js/app.js
@@ -1,1 +1,9 @@
 jQuery(document).foundation();
+jQuery(function($){
+    $('#filtrar .menu a').on('click',function(){
+        var enlace = $(this).attr('href');
+        $('#platillos > div').hide();
+        $(enlace).fadeIn();
+        return false;
+    });
+});
