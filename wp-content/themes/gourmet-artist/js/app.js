40014279635b936beb1e6cda1e06ef78dc20083c
Miguel Zac <miguel.quispe@frctal.com.pe>
2018-11-15 11:01:11 -0500

    38. Finalizando nuestro Ejemplo con CSS.

--- wp-content/themes/gourmet-artist/js/app.js
+++ wp-content/themes/gourmet-artist/js/app.js
@@ -1,6 +1,11 @@
 jQuery(document).foundation();
 jQuery(function($){
+    $('#platillos > div').not(':first').hide();
+    $('#filtrar .menu li:first-child').addClass('active');
+    
     $('#filtrar .menu a').on('click',function(){
+        $('#filtrar .menu li').removeClass('active');
+        $(this).parent().addClass('active');
         var enlace = $(this).attr('href');
         $('#platillos > div').hide();
         $(enlace).fadeIn();
