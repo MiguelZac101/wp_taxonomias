b0e23ccd1b84782d1ba4bec807d5f8897be96a9f
Miguel Zac <miguel.quispe@frctal.com.pe>
2018-11-20 17:21:24 -0500

    49. Guardando Metaboxes a la BD.

--- wp-content/plugins/ga-metaboxes/ga-metaboxes.php
+++ wp-content/plugins/ga-metaboxes/ga-metaboxes.php
@@ -17,30 +17,73 @@ function ga_agregar_metaboxes() {
 
 add_action("add_meta_boxes", "ga_agregar_metaboxes");
 
+function ga_guardar_metaboxes($post_id, $post, $update) {
+    if (!isset($_POST["meta-box-nonce"]) || !wp_verify_nonce($_POST["meta-box-nonce"], basename(__FILE__)))
+        return $post_id;
+
+    if (!current_user_can("edit_post", $post_id))
+        return $post_id;
+
+    if (defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+        return $post_id;
+
+    $input_metabox = "";
+    $textarea_metabox = "";
+    $dropdown_metabox = "";
+
+    if (isset($_POST["input-metabox"])) {
+        $input_metabox = $_POST["input-metabox"];
+    }
+    update_post_meta($post_id, "input-metabox", $input_metabox);
+
+    if (isset($_POST["textarea-metabox"])) {
+        $textarea_metabox = $_POST["textarea-metabox"];
+    }
+    update_post_meta($post_id, "textarea-metabox", $textarea_metabox);
+
+
+    if (isset($_POST["dropdown-metabox"])) {
+        $dropdown_metabox = $_POST["dropdown-metabox"];
+    }
+    update_post_meta($post_id, "dropdown-metabox", $dropdown_metabox);
+}
+
+add_action("save_post", "ga_guardar_metaboxes", 10, 3);
+
 function ga_diseno_metaboxes($post) {
-    wp_nonce_field(basename(__FILE__),"meta-box-nonce");
-?>
-<div>
-    <label for="input-metabox">Calorias</label>
-    <input name="input-metabox" type="text">
-    <br/>
-    
-    <label for="textarea-metabox">subtitulo de la receta:</label>
-    <textarea name="textarea-metabox"></textarea>
-    <br/>
-    
-    <label for="dropdown-metabox">calificación:</label>
-    <select name="dropdown-metabox">
-        <?php 
-        $opciones = array(1,2,3,4,5);
-        foreach($opciones as $llave => $valor){
-        ?>
-        <option><?php echo $valor; ?></option>
-        <?php            
-        }
-        ?>
-    </select>
-    
-</div>
-<?php
+    wp_nonce_field(basename(__FILE__), "meta-box-nonce");
+    ?>
+    <div>
+        <label for="input-metabox">Calorias</label>
+        <input name="input-metabox" type="text" value="<?php echo get_post_meta($post->ID, "input-metabox", true); ?>">
+        <br/>
+
+        <label for="textarea-metabox">subtitulo de la receta:</label>
+        <textarea name="textarea-metabox">
+            <?php echo get_post_meta($post->ID, "textarea-metabox", true); ?>
+        </textarea>
+        <br/>
+
+        <label for="dropdown-metabox">calificación:</label>
+        <select name="dropdown-metabox">
+            <?php
+            $seleccion = get_post_meta($post->ID, "dropdown-metabox", true);
+            
+            $opciones = array(1, 2, 3, 4, 5);
+            foreach ($opciones as $llave => $valor) {
+                if($valor == $seleccion){
+                ?>
+                <option selected><?php echo $valor; ?></option>
+                <?php    
+                }else{
+                 ?>
+                <option><?php echo $valor; ?></option>
+                <?php   
+                }                
+            }
+            ?>
+        </select>
+
+    </div>
+    <?php
 }
